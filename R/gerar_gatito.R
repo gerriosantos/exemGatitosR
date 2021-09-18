dfccd11b903a236f4c612284fdc46b2c19f808e1
gerriosantos <gerriosantos@gmail.com>
2021-09-18 18:01:15 -0300

    enviar func

---
+++ R/gerar_gatito.R
@@ -0,0 +1,19 @@
+
+#' Gerar Gatito
+#'
+#' @return Nào retorna nada
+#' @export
+#'
+#'
+gerar_gatito <- function(){
+  tmp <- tempfile(fileext = '.jpeg')
+  utils::download.file('https://cataas.com/cat',
+                       temp, quiet = TRUE)
+
+  tmp |>
+    jpeg::readJPEG() |>
+    grDevices::as.raster() |>
+    graphics::plot()
+}
+
+utils::globalVariables(c('temp'))
